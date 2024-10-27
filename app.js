1f2017cc791df94025857772f9caae13afb38076
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-10-27 11:31:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// Question No. 1
+// While LOOP
+// var i = 0;
+// while (i <= 3) {
+// alert(i);
+// i++;
+// }
+
+// Question No.2
+// Do While LOOP
+// var i = 0;
+// do {
+// alert(i);
+// i++;
+// } while (i <= 3);
+
+// Question No.3
+// Check Index Documentry File
+
+// Question No.4
+// Js Coment Syntax
+/*  Css Coment Syntax */
+/* When a comment requires more than one line,
+a block comment like this, with its opening
+// and closing tags, is the way to go.*/ //(also used in JS for comenting multiple lines)
+
+// <!-- --> Html Coment Syntax
+
+// Question No.5
+
+
+
